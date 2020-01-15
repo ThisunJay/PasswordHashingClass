4099e3291be020393fe2744a465c21e3a11d65f6
ThisunJay <jayamaduthisun@gmail.com>
2020-01-15 21:55:40 +0530

    console app with class .net core project 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hash
+            // var hash = SecurePasswordHasher.Hash("mypassword");
+
+            // Verify
+            //var result = SecurePasswordHasher.Verify("mypassword", hash);
+
+            Console.Write("Enter password: ");
+            string pwd = Console.ReadLine();
+
+            var hash = SecurePasswordHasher.Hash(pwd);
+            Console.WriteLine("Hashed password = {0}", hash);
+
+            char usin;
+
+            Console.Write("Wanna veryfiy password: y/n?");
+            usin = Console.ReadKey().KeyChar;
+
+            if (usin == 'y')
+            {
+                Console.Write("Enter password to verify: ");
+                string vpwd = Console.ReadLine();
+
+                var result = SecurePasswordHasher.Verify(vpwd, hash);
+
+                if (result is true)
+                {
+                    Console.WriteLine("Password is Correct");
+                }
+                else
+                    Console.WriteLine("Password is Wrong");
+
+            }
+            else {
+                Console.WriteLine("Your password was = {0}", pwd);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
